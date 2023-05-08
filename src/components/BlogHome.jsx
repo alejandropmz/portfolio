0c81d874926c88474b468f46aaaa6e2951b57db4
alejandropmz <alejandropacheco2497@gmail.com>
2023-05-07 19:01:36 -0500

    initialized blog

--- src/components/BlogHome.jsx
+++ src/components/BlogHome.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { MiniViewBlog } from "./MiniViewBlog";
 
 export const BlogHome = () => {
   return (
     <div id="blog-home-container">
-      <div className="general-padding">BlogHome</div>
+      <div className="general-padding">
+        <h1 className="text-center">Lee un poco sobre lo que escribo...</h1>
+        <div className="d-flex">
+          <MiniViewBlog />
+          <MiniViewBlog />
+          <MiniViewBlog />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/MiniViewBlog.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const MiniViewBlog = () => {
+  return (
+    <div id="mini-view-container">
+      <img
+        src="https://media.istockphoto.com/id/1198931639/photo/writing-a-blog-blogger-influencer-reading-text-on-screen.jpg?s=612x612&w=0&k=20&c=4FJ_fzzZYqBoGG-RY8fcohpaOKKwnnI-ik58cPy6t-g="
+        alt=""
+      />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, assumenda
+        deleniti. Suscipit nobis voluptatem, asperiores placeat saepe non cum
+        laudantium sapiente itaque. Aperiam perferendis rem soluta quisquam
+        molestiae, unde reiciendis!
+      </p>
+      <button>Lee más</button>
+    </div>
+  );
+};
