79ce6dade58c0c0bc0c507302169c0258e4db442
alejandropmz <alejandropacheco2497@gmail.com>
2023-05-09 17:50:00 -0500

    blog home view

--- src/components/BlogHome.jsx
+++ src/components/BlogHome.jsx
@@ -6,10 +6,42 @@ export const BlogHome = () => {
     <div id="blog-home-container">
       <div className="general-padding">
         <h1 className="text-center">Lee un poco sobre lo que escribo...</h1>
-        <div className="d-flex">
+        <div id="no-carousel">
           <MiniViewBlog />
           <MiniViewBlog />
           <MiniViewBlog />
+          <MiniViewBlog />
+        </div>
+        <div id="carouselExample" class="carousel slide blog-carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <MiniViewBlog />
+            </div>
+            <div class="carousel-item">
+              <MiniViewBlog />
+            </div>
+            <div class="carousel-item">
+              <MiniViewBlog />
+            </div>
+          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
       </div>
     </div>
--- src/components/MiniViewBlog.jsx
+++ src/components/MiniViewBlog.jsx
@@ -2,18 +2,23 @@ import React from "react";
 
 export const MiniViewBlog = () => {
   return (
-    <div id="mini-view-container">
+    <div className="card" style={{ width: "18rem" }}>
       <img
-        src="https://media.istockphoto.com/id/1198931639/photo/writing-a-blog-blogger-influencer-reading-text-on-screen.jpg?s=612x612&w=0&k=20&c=4FJ_fzzZYqBoGG-RY8fcohpaOKKwnnI-ik58cPy6t-g="
-        alt=""
+        src="https://recursosmarketing.net/wp-content/uploads/2022/07/blog-como-recurso-de-marketing-840x420.png"
+        className="card-img-top"
+        alt="..."
       />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, assumenda
-        deleniti. Suscipit nobis voluptatem, asperiores placeat saepe non cum
-        laudantium sapiente itaque. Aperiam perferendis rem soluta quisquam
-        molestiae, unde reiciendis!
-      </p>
-      <button>Lee más</button>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <button id="read-more">
+          Leer más
+          <i className="bx bx-right-arrow-alt"></i>
+        </button>
+      </div>
     </div>
   );
 };
