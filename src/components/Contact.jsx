5954ca307abc3d624224665151858d4db3e692bd
alejandropmz <alejandropacheco2497@gmail.com>
2023-05-10 22:16:55 -0500

    footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,46 @@ import React from "react";
 export const Contact = () => {
   return (
     <div id="contact-container">
-      <div className="general-padding">Contact</div>
+      <div className="general-padding">
+        <div id="footer-header" className="d-flex justify-content-center">
+          <h3>Envíame un mensaje</h3>
+        </div>
+        <div id="contact-form" className="row">
+          <div className="col-sm-12 col-md-6">
+            <h5>Trabajemos en equipo!</h5>
+            <p>Bogotá, Colombia</p>
+            <p>alejandropacheco2497@gmail.com</p>
+            <div id="social-icons">
+              <a href="">
+                <i className="bx bxl-github"></i>
+              </a>
+              <a href="">
+                <i className="bx bxl-linkedin"></i>
+              </a>
+              <a href="">
+                <i className="bx bxl-youtube"></i>
+              </a>
+              <a href="">
+                <i className="bx bxl-twitter"></i>
+              </a>
+              <a href="">
+                <i className="bx bx-envelope"></i>
+              </a>
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-6">
+            <form id="footer-form">
+              <input placeholder="Nombres" type="text" />
+              <br />
+              <input placeholder="Correo electrónico" type="text" />
+              <br />
+              <input placeholder="Mensaje" type="text" />
+              <br />
+              <button type="submit">Enviar</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
